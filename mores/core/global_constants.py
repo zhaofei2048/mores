bc1a1f4ed411f42e94a8c9e53ca6876afa992afd
Fei Zhao <2550688560@qq.com>
2024-01-16 15:35:07 +0800

    Change the model folder name

--- mores/core/global_constants.py
+++ mores/core/global_constants.py
@@ -5,3 +5,7 @@ Inspired by and adapted from SMRT: https://github.com/smrt-model/smrt.
 Author: Fei Zhao
 Create: 2024-01-16
 """
+
+import scipy.constants
+
+SPEED_OF_LIGHT = scipy.constants.speed_of_light
---
+++ mores/interface/IEM.py
@@ -0,0 +1,4 @@
+from ..core.global_constants import SPEED_OF_LIGHT
+# from python.core.global_constants import SPEED_OF_LIGHT
+
+print(f"speed of light is:{SPEED_OF_LIGHT}")
